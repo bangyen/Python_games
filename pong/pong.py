124b5342e5cac7f21783e67d9dc8bd9edb499ffc
kristofer <kristofergauti@gmail.com>
2020-06-07 00:49:16 +0000

    Added a better path finding with the os module

--- pong/pong.py
+++ pong/pong.py
@@ -97,6 +97,7 @@ def collide(obj1, obj2):
     offset_y = obj2.y - obj1.y  
     return obj1.mask.overlap(obj2.mask, (int(offset_x), int(offset_y))) != None # (x,y)
 
+
 def main():
     run = True
     FPS = 60
